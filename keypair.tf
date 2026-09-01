c53c71d1e3c3dbb4092be030b2122fcd912b00be
Abdul Rahim <raheemabdur106@gmail.com>
2026-09-02 01:25:26 +0500

    divided code into file

---
+++ keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "terraform_web_server" {
+  key_name = "terraform-web-server"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICkxERKd7LGvhDJPbS9ebYLHs0zTAiWXGovvpIH1MPDn abdul@CodeTitan"
+}
